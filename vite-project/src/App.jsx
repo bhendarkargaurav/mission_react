14dd03ed412d58b36a5791d0ec9e189ff276fe87
Gaurav <gaurav.bhe.24@gmail.com>
2026-05-29 16:44:52 +0530

    exploring some fundamental for react , tailwind and much more

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,23 @@
-function app() {
-  return <h1>"Hellow from app.jsx"</h1>
+import React from "react";
+
+const App = () => {
+
+  const submitHandler = () => {
+    console.log("Hello")
+  }
+
+  return (
+    <div> 
+
+      <form onSubmit={submitHandler}>
+        <input className='px-4 py-3 text-xl m-5 text-white' type="text" placeholder=" Enter your input"/>
+        <button className="px-4 py-3 m-5 text-xl font-semibolt bg-emerald-600">Submit</button>
+      </form>
+     
+    </div>
+  )
 }
 
-export default app;
+export default App
+
+
