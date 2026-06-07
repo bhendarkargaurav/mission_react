2411239582649cd0dd71c7e735e6cef87e509000
Gaurav <gaurav.bhe.24@gmail.com>
2026-06-07 23:42:19 +0530

    admin dashboard is ready

--- Employee management system/src/components/TaskList/TaskList.jsx
+++ Employee management system/src/components/TaskList/TaskList.jsx
@@ -48,7 +48,7 @@ const TaskList = () => {
             <p className='text-sm mt-2'> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, sit! Officia quo ea animi vero?</p>
         </div>
 
-        <div className='flex-shrink-0 rounded-xl h-full w-[300px] p-5 bg-yellow-400'>
+        <div className='shrink-0 rounded-xl h-full w-75 p-5 bg-yellow-400'>
             <div className='flex justify-between items-center'>
                 <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High</h3>
                 <h4 className='text-sm'>20 feb 2024</h4>
---
+++ Employee management system/src/components/other/AllTask.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 mt-5 h-60 overflow-auto rounded'>
+        <div className='bg-red-400 mb-1 py-2 px-4 flex justify-between'>
+            <h2>sarthak</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 mb-1 py-2 px-4 flex justify-between'>
+            <h2>sarthak</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 mb-1 py-2 px-4 flex justify-between'>
+            <h2>sarthak</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 mb-1 py-2 px-4 flex justify-between'>
+            <h2>sarthak</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 mb-1 py-2 px-4 flex justify-between'>
+            <h2>sarthak</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-purple-400 mb-1 py-2 px-4 flex justify-between'>
+            <h2>sarthak</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
