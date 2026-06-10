25460475d801933f26e9076d83fc3cd9380894f9
Gaurav <gaurav.bhe.24@gmail.com>
2026-06-10 15:14:47 +0530

    manage app.jsx

--- Employee management system/src/App.jsx
+++ Employee management system/src/App.jsx
@@ -5,19 +5,42 @@ import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/LocalStorage'
 import { useState } from 'react'
 import { AuthContext } from './context/AuthProvider'
+import AuthProvider from './context/AuthProvider'
 
 const App = () => {
 
   const [user, setUser] = useState(null)
+  const [loggedInUserData, setloggedInUserData] = useState(null)
 
    const authData = useContext(AuthContext)
-   console.log(authData.employee[0])
+
+  
+   
+  // set who is logedin, user or admin
+  // useEffect(() => {
+  //   if(authData) {
+  //     const loggedInUser = localStorage.getItem("loggedInUser")
+  //     if(loggedInUser) {
+  //       setUser(loggedInUser.role)
+  //     }
+
+  //   }
+  // }, [authData])
+  
+
+
 
   const handleLogin = (email, password) => {
     if(email == 'admin@me.com' && password == '123') {
       setUser('admin')
-    }else if(email=='user@me.com' && password == '123') {
-      setUser('employee')
+      localStorage.setItem('loggedInUser', JSON.stringify({role:'admin'}))
+    }else if(authData) {
+      const employee = authData.employees.find((e) => email == e.email && password == e.password)
+      if(employee) {
+        setUser('employee')
+        setloggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({role:'employee'}))
+      }
     }
     else {
       alert("Invalid Credentials")
@@ -31,7 +54,7 @@ const App = () => {
     <>
    // we want as some one login accourding to dashboard should open if employee login employee dashboard open and if admin login admin dashboad open 
     {!user ? < Login handleLogin={handleLogin} />: ''}
-    {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard />}
+    {user == 'admin' ? <AdminDashboard/> : (user == 'employee' ? <EmployeeDashboard data={loggedInUserData} /> :null)}
 
     {/* <EmployeeDashboard /> */}
     {/* < AdminDashboard /> */}
