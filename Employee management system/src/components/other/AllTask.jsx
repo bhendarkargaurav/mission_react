80c0944242f15408d6c8ccb7ac9c5c8b76563acc
Gaurav <gaurav.bhe.24@gmail.com>
2026-06-16 22:26:04 +0530

    updateion alltask

--- Employee management system/src/components/other/AllTask.jsx
+++ Employee management system/src/components/other/AllTask.jsx
@@ -1,38 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
+
+    const authData = useContext(AuthContext)
+
+
   return (
-    <div className='bg-[#1c1c1c] p-5 mt-5 h-60 overflow-auto rounded'>
-        <div className='bg-red-400 mb-1 py-2 px-4 flex justify-between'>
-            <h2>sarthak</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
-        </div>
+    <div className='bg-[#1c1c1c] p-5 mt-5 h-65  rounded'>
         <div className='bg-blue-400 mb-1 py-2 px-4 flex justify-between'>
-            <h2>sarthak</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
+            <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
+            <h3 className='text-lg font-medium w-1/5'>New Task</h3>
+            <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
+            <h5 className='text-lg font-medium w-1/5'>Completed</h5>
+            <h5 className='text-lg font-medium w-1/5'>Failed</h5>
         </div>
-        <div className='bg-red-400 mb-1 py-2 px-4 flex justify-between'>
-            <h2>sarthak</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
+        <div className=' '>
+            {authData.employees.map(function(elem, idx) {
+            return <div key={idx} className='border-emerald-400 border-2 mb-2 py-2 px-4 flex justify-between'>
+            <h2 className='text-lg font-medium w-1/5'>{elem.firstName}</h2>
+            <h3 className='text-lg font-medium w-1/5 text-blue-600'>{elem.taskCounts.newTask}</h3>
+            <h5 className='text-lg font-medium w-1/5 text-yellow-600'>{elem.taskCounts.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskCounts.completed}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
         </div>
-        <div className='bg-blue-400 mb-1 py-2 px-4 flex justify-between'>
-            <h2>sarthak</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
+
+        })}
         </div>
-        <div className='bg-red-400 mb-1 py-2 px-4 flex justify-between'>
-            <h2>sarthak</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-purple-400 mb-1 py-2 px-4 flex justify-between'>
-            <h2>sarthak</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
-        </div>
+        
+    
     </div>
   )
 }
