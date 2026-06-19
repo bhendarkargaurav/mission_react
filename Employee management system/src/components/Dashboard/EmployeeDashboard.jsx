e98a81a28887c4b568d53d9f1749b661545bb909
Gaurav <gaurav.bhe.24@gmail.com>
2026-06-19 13:35:23 +0530

    employee dashboard

--- Employee management system/src/components/Dashboard/EmployeeDashboard.jsx
+++ Employee management system/src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,14 +3,14 @@ import Header from '../other/Header'
 import TaskNumberList from '../other/TaskNumberList'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = (data) => {
+const EmployeeDashboard = (props) => {
   // console.log(props)
   return (
     <div className='p-10 bg-[#1c1c1c] h-screen'>
       
-        <Header data={data} />
-        <TaskNumberList data={data} />
-        <TaskList data={data} />
+        <Header changeuser={props.changeuser} data={props.data} />
+        <TaskNumberList data={props.data} />
+        <TaskList data={props.data} />
     </div>
   )
 }
